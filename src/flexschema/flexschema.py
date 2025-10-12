0ae4ec4ab3be08f103b575e3bea9974115ac3913
Gino D. <25200363+zinosaure@users.noreply.github.com>
2025-10-13 00:17:02 +0200

    Whitespaces trimmed

--- src/flexschema/flexschema.py
+++ src/flexschema/flexschema.py
@@ -181,7 +181,7 @@ class Flex:
     def update(self, **data: Any):
         for name, field in self.schema:
             value = data.get(name, self.__dict__.get(name, field.default))
-            
+
             if issubclass(field.type, Flexmodel) and isinstance(value, dict) and (id := value.get("$id")):
                 value = field.type.load(id)
             elif issubclass(field.type, Flex) and isinstance(value, dict):
