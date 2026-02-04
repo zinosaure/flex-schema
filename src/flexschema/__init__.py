8c4ff258fa58e35ba985068554929f17137ebc2a
Gino D. <25200363+zinosaure@users.noreply.github.com>
2026-02-04 21:00:05 +0100

    refactor: Update ORM-style example and remove FlexmodelLite implementation

--- src/flexschema/__init__.py
+++ src/flexschema/__init__.py
@@ -1,3 +1,2 @@
-from .flexschema import Schema, Flex, Flexmodel, field, field_constraint  # noqa: F401
-from .flexschema_lite import FlexmodelLite, FlexmodelLiteException  # noqa: F401
+from .flexschema import Schema, Flex, Flexmodel, FlexmodelLite, field, field_constraint  # noqa: F401
 from .version import __version__  # noqa: F401
